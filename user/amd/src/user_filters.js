2133ede926cb621e95389b3500dfa3916722c0eb
Michael Hawkins <michaelh@moodle.com>
2020-02-28 23:45:28 +0800

    WIP

--- user/amd/src/user_filters.js
+++ user/amd/src/user_filters.js
@@ -22,15 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-//TODO: CLEANUP imports
-//import $ from 'jquery';
-//import Popper from 'core/popper';
-//import CustomEvents from 'core/custom_interaction_events';
-//import Selectors from 'forumreport_summary/selectors';
-//import Y from 'core/yui';
-//import Ajax from 'core/ajax';
-//import KeyCodes from 'core/key_codes';
-
 export const init = (uniqid) => {
 
 //TODO: Most of this is probably generic enough that it can be initialised in the outer filter,
@@ -42,33 +33,38 @@ export const init = (uniqid) => {
 
     // Handler for setting values for single dropdowns.
     const setDropdownValue = e => {
-        const dropdownType = e.target.parentNode.parentNode.querySelector('button').id;
+        e.preventDefault();
+
+        const dropdown = e.target;
+        const dropdownType = dropdown.parentNode.parentNode.querySelector('button').id;
 
         // Display the selection and set the hidden value.
-        document.getElementById(dropdownType).innerText = e.target.innerText;
-        document.getElementById(`${dropdownType}-selected`).value = e.target.getAttribute('data-value');
+        document.getElementById(dropdownType).innerText = dropdown.innerText;
+        document.getElementById(`${dropdownType}-selected`).value = dropdown.getAttribute('data-value');
+
+        // Filter type dropdowns need to display the filter options.
+        if (dropdown.hasAttribute("data-filter-options-id")) {
+            const optionsid = dropdown.getAttribute("data-filter-options-id");
+
+            document.getElementById(optionsid).classList.add('d-inline-block');
+            document.getElementById(optionsid).classList.remove('hidden');
+        }
 
     };
 
-    // Handler which displays the input option(s) available for a chosen filter type.
-    const displayFilterValueInput = e => {
-        //TODO
-    };
+    const setOptionValue = e => {
+        const value = e.target.getAttribute('data-value');
+        let valueTag = document.createElement('span');
+        let crossicon = document.createElement('i');
+        //TODO: Figure out if we need to store the values somewhere hidden as well, for easy sending in the web service
 
-    // Set listener on each filter dropdown menu.
-    document.querySelectorAll('.dropdown-menu.user-filter-select').forEach((dropdown) => {
-        dropdown.addEventListener('click', setDropdownValue);
-    });
-
-    // Set listener for displaying filter input option(s).
-    document.querySelectorAll('.filter-type-value').forEach((filterTypeValue) => {
-        filterTypeValue.addEventListener('change', displayFilterValueInput);
-    });
-
-    const chooseOption = e => {
-        window.console.log(e.target.getAttribute('data-value'));
-        //TODO: This is fetching the correct value. The template will need a select to replace the "type or select"
-                //with in the UI, and then re-appear if they click it?
+        // Set up and display new tag for chosen value.
+        valueTag.setAttribute('data-value', value);
+        valueTag.classList.add('d-inline-block', 'bg-dark', 'text-white', 'rounded', 'font-weight-bold', 'px-1');
+        valueTag.innerText = e.target.innerText;
+        crossicon.classList.add('icon', 'fa', 'fa-times', 'pl-2', 'mr-0');
+        valueTag.appendChild(crossicon);
+        document.getElementById(`${uniqid}-filter-options-set`).appendChild(valueTag);
     };
 
     const toggleFilterDropdown = e => {
@@ -78,12 +74,18 @@ export const init = (uniqid) => {
                 //there's also the case of the value being selected on the next dropdown, which adds the
                 //little removable tag bits. Need to see if they can be differentiated (maybe a data- tag) that
             //allows generic code here to set one of two different event listeners, depending which type it is.
-        dropdownList.addEventListener('click', chooseOption);
+        dropdownList.addEventListener('click', setOptionValue);
 
         dropdownList.classList.remove('hidden');
     };
 
-    const filterdiv = document.getElementById(`${uniqid}-TODOfilter-types`);
+    // Set listener on each filter dropdown menu.
+    document.querySelectorAll('.dropdown-menu.user-filter-select').forEach((dropdown) => {
+        dropdown.addEventListener('click', setDropdownValue);
+    });
+
+    // Add listeners for 'type or select' filter options type.
+    const filterdiv = document.getElementById(`${uniqid}-filter-options`);
 
     filterdiv.querySelector('input[type="text"]').addEventListener('focus', toggleFilterDropdown);
     filterdiv.querySelector('span').addEventListener('click', toggleFilterDropdown);
