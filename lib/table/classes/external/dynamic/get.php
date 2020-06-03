2ecad306f4fefdab38d1cbe6f4c1e14bdd0cd122
Michael Hawkins <michaelh@moodle.com>
2020-06-03 17:19:47 +0800

    MDL-68745 lib: Updated table filters to support non-English characters
    Utilised the new language-based alphabet escaping to support first/last
    name filtering in languages with non a-z character sets.

--- lib/table/classes/external/dynamic/get.php
+++ lib/table/classes/external/dynamic/get.php
@@ -98,13 +98,13 @@ class get extends external_api {
             ),
             'jointype' => new external_value(PARAM_INT, 'Type of join to join all filters together', VALUE_REQUIRED),
             'firstinitial' => new external_value(
-                PARAM_ALPHANUMEXT,
+                PARAM_LANGALPHANUMEXT,
                 'The first initial to sort filter on',
                 VALUE_REQUIRED,
                 null
             ),
             'lastinitial' => new external_value(
-                PARAM_ALPHANUMEXT,
+                PARAM_LANGALPHANUMEXT,
                 'The last initial to sort filter on',
                 VALUE_REQUIRED,
                 null
