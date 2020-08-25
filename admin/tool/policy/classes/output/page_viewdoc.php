c8003936f742d9a74eae771ebb995c8f7a7e06bb
Michael Hawkins <michaelh@moodle.com>
2020-08-25 13:01:44 +0800

    MDL-69495 tool_policy: Move policy count string rendering to renderer
    This is required now the str helper no longer renders the nested
    JSON variables. The previous use of quote helper also appaered to
    perform no modification to the content.

--- admin/tool/policy/classes/output/page_viewdoc.php
+++ admin/tool/policy/classes/output/page_viewdoc.php
@@ -153,11 +153,21 @@ class page_viewdoc implements renderable, templatable {
     public function export_for_template(renderer_base $output) {
         global $USER;
 
+        if ($this->numpolicy) {
+            $countvars = [
+                'numpolicy' => $this->numpolicy,
+                'totalpolicies' => $this->totalpolicies,
+            ];
+            $policycountstring = get_string('steppolicies', 'tool_policy', $countvars);
+        } else {
+            $policycountstring = null;
+        }
+
         $data = (object) [
             'pluginbaseurl' => (new moodle_url('/admin/tool/policy'))->out(false),
             'returnurl' => $this->returnurl ? (new moodle_url($this->returnurl))->out(false) : null,
             'numpolicy' => $this->numpolicy ? : null,
-            'totalpolicies' => $this->totalpolicies ? : null,
+            'policycountstring' => $policycountstring,
         ];
         if ($this->manage && $this->policy->status != policy_version::STATUS_ARCHIVED) {
             $paramsurl = ['policyid' => $this->policy->policyid, 'versionid' => $this->policy->id];
