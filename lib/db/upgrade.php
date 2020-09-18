e120b3353c4b8ec2cd6a9812b989e8213375e312
Michael Hawkins <michaelh@moodle.com>
2020-09-18 18:52:13 +0800

    MDL-69559 course: Add course setting and cap to control course downloads

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2694,5 +2694,18 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2021052500.13);
     }
 
+    if ($oldversion < 2021052500.15) {
+        // Define field contentdownload to be added to course.
+        $table = new xmldb_table('course');
+        $field = new xmldb_field('contentdownload', XMLDB_TYPE_INTEGER, '1', null, null, null, null, 'visibleold');
+
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2021052500.15);
+    }
+
     return true;
 }
