bcddbb6373af64804d956a75105bb7e80cda6d2a
Simey Lameze <simey@moodle.com>
2021-03-02 07:02:25 +0800

    MDL-70820 mod_glossary: custom completion implementation

---
+++ mod/glossary/classes/completion/custom_completion.php
@@ -0,0 +1,82 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Activity custom completion subclass for the glossary activity.
+ *
+ * Contains the class for defining mod_glossary's custom completion rules and fetching the completion statuses
+ * of the custom completion rules for a given glossary instance and a user.
+ *
+ * @package mod_glossary
+ * @copyright Simey Lameze <simey@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+declare(strict_types = 1);
+
+namespace mod_glossary\completion;
+
+use core_completion\activity_custom_completion;
+
+/**
+ * Activity custom completion subclass for the glossary activity.
+ *
+ * Class for defining mod_glossary's custom completion rules and fetching the completion statuses
+ * of the custom completion rules for a given glossary instance and a user.
+ *
+ * @package mod_glossary
+ * @copyright Simey Lameze <simey@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class custom_completion extends activity_custom_completion {
+
+    /**
+     * Fetches the completion state for a given completion rule.
+     *
+     * @param string $rule The completion rule.
+     * @return int The completion state.
+     */
+    public function get_state(string $rule): int {
+        global $DB;
+
+        $this->validate_rule($rule);
+
+        $status = $DB->record_exists('glossary_entries', ['glossaryid' => $this->cm->instance, 'userid' => $this->userid, 'approved' => 1]);
+        return $status ? COMPLETION_COMPLETE : COMPLETION_INCOMPLETE;
+    }
+
+    /**
+     * Fetch the list of custom completion rules that this module defines.
+     *
+     * @return array
+     */
+    public static function get_defined_custom_rules(): array {
+        return [
+            'completionsubmit'
+        ];
+    }
+
+    /**
+     * Returns an associative array of the descriptions of custom completion rules.
+     *
+     * @return array
+     */
+    public static function get_custom_rule_descriptions(): array {
+        return [
+            'completionsubmit' => get_string('completiondetail:submit', 'glossary')
+        ];
+    }
+}
