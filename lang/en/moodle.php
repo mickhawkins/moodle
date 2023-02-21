b132dbd00172324bbf633ac5a1f42e2528ad31f8
Michael Hawkins <michaelh@moodle.com>
2023-02-22 02:39:02 +0800

    MDL-75932 core: Add core API for sharing activities to MoodleNet

--- lang/en/moodle.php
+++ lang/en/moodle.php
@@ -1364,6 +1364,7 @@ $string['moodleversion'] = 'Moodle version';
 $string['moodlerelease'] = 'Moodle release';
 $string['moodleservicesandsupport'] = 'Services and support';
 $string['moodlenet:configoauthservice'] = 'Select the OAuth 2 service that is configured to talk to the MoodleNet server. If the service doesn\'t exist yet, you will need to <a href="{$a}">create</a> it.';
+$string['moodlenet:eventresourceexported'] = 'Resource exported';
 $string['moodlenet:outboundsettings'] = 'MoodleNet outbound settings';
 $string['more'] = 'more';
 $string['morehelp'] = 'More help';
