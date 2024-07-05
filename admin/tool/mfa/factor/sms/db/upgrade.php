69091f17384b58dd2d6e79f78707d0427ee30da2
Safat <safat.shahin@moodle.com>
2024-07-05 16:38:46 +1000

    MDL-80962 factor_sms: Remove leftover AWS implementation

--- admin/tool/mfa/factor/sms/db/upgrade.php
+++ admin/tool/mfa/factor/sms/db/upgrade.php
@@ -59,6 +59,18 @@ function xmldb_factor_sms_upgrade(int $oldversion): bool {
         // MFA savepoint reached.
         upgrade_plugin_savepoint(true, 2024082200, 'factor', 'sms');
     }
+    if ($oldversion < 2024070500) {
+        // Unset the removed admin settings.
+        unset_config('countrycode', 'factor_sms');
+        unset_config('gateway', 'factor_sms');
+        unset_config('usecredchain', 'factor_sms');
+        unset_config('api_key', 'factor_sms');
+        unset_config('api_secret', 'factor_sms');
+        unset_config('api_region', 'factor_sms');
+
+        // MFA savepoint reached.
+        upgrade_plugin_savepoint(true, 2024070500, 'factor', 'sms');
+    }
 
     return true;
 }
